1b625d8c07163c496b1775892b9a178f578131d6
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2025-06-18 11:56:13 +0530

    Testing of prompt using a POST request

--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -1,11 +1,11 @@
 const aiService = require("../services/ai.service");
 
-module.exports.getResponse = async (req, res) => {
-  const { prompt } = req.query;
-  if (!prompt) {
+module.exports.getReview = async (req, res) => {
+  const { code } = req.body;
+  if (!code) {
     return res.status(400).send("Prompt is required");
   }
-  const response = await aiService(prompt);
+  const response = await aiService(code);
 
   res.send(response);
 };
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -2,12 +2,14 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-// const prompt = "Explain how AI works";
-
-// const result = await model.generateContent(prompt);
-// console.log(result.response.text());
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+  systemInstruction: `
+    You are an code reviewer, who have an expertise in development,
+    you look for the code and find the problems and suggest the solution
+    to the developer.
+    `,
+});
 
 async function generateContent(prompt) {
   const result = await model.generateContent(prompt);
