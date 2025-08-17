5341b32ab3463eda1dd08fbdeea2effedd2908f9
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2025-08-17 13:00:35 +0530

    Changes Made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [ count, setCount ] = useState(0)
+  // const [ count, setCount ] = useState(0)
   const [ code, setCode ] = useState(` function sum() {
   return 1 + 1
 }`)
